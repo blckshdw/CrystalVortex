3e3d05b354735b72acc793aaf65682c5609176be
blckshdw <danhaas@gmail.com>
2015-09-11 18:59:24 -0400

    Add Contact form

--- CrystalVortex/Controllers/HomeController.cs
+++ CrystalVortex/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using CrystalVortex.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,14 +19,37 @@ namespace CrystalVortex.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = "";
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Contact(ContactModel c)
+        {
+            if (ModelState.IsValid)
+            {
+                var emailTo = new MailAddress(ConfigurationManager.AppSettings["ContactEmail"].ToString());
+                var emailFrom = new MailAddress(ConfigurationManager.AppSettings["SmtpFrom"].ToString());
+                var smtpHost = ConfigurationManager.AppSettings["SmtpHost"].ToString();
+
+                var client = new SmtpClient(smtpHost);
+                var msg = new MailMessage();
+                msg.From = emailFrom;
+                msg.ReplyToList.Add(new MailAddress(c.EmailAddress));
+                msg.To.Add(emailTo);
+                msg.Subject = "Crystal Vortex Feedback";
+                msg.Body = String.Format("Message From: {0} {1}\nMessage Submitted {2}\n\n{3}", c.FirstName, c.LastName, DateTime.UtcNow.ToString("R"), c.Comments);
+                msg.IsBodyHtml = false;
+
+                await client.SendMailAsync(msg);
+
+                return View("ContactSubmitted");
+            }
 
             return View();
         }
---
+++ CrystalVortex/Models/ContactModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalVortex.Models
+{
+    public class ContactModel
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string Comments { get; set; }
+
+    }
+}
+
