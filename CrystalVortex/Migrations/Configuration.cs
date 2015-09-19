40a3c4f8e5f9259878fd21638f7eaa2cca8bfd3c
u4ic <newnstar@hotmail.com>
2015-09-19 18:23:23 -0400

    allow migrationdataloss

--- CrystalVortex/Migrations/Configuration.cs
+++ CrystalVortex/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace CrystalVortex.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(CrystalVortex.Models.ReleaseModel context)
